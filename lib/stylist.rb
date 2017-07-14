8102c8cf46aa2a7e4fb07d3348b65889107a263c
kiburei <akiburei@gmail.com>
2017-07-14 11:29:51 +0300

    pass spec to initialize new class instance

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,7 +1,18 @@
-class stylist
+class Stylist
   attr_accessor(:name, :id)
+
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
+
+  def self.all
+    results = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    results.each do |stylist|
+      stylists.push(Stylist.new(name: stylist.fetch('name', id: stylist.fetch('id'))))
+    end
+    stylists
+  end
+
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,3 +1,21 @@
 require "rspec"
 require "stylist"
 require "pg"
+
+DB = PG.connect(dbname: 'hair_salon_test')
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM stylists *;")
+  end
+end
+
+describe(Stylist) do
+
+  describe('.all') do
+    it('starts off with no stylists') do
+      expect(Stylist.all).to(eq([]))
+    end
+  end
+
+end
