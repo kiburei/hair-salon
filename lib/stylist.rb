cc460849ef65daf031b7c339af96f0d03ee5158e
kiburei <akiburei@gmail.com>
2017-07-14 12:02:44 +0300

    add CRUD methods to call style

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -6,6 +6,7 @@ class Stylist
     @id = attributes.fetch(:id)
   end
 
+# return everything from class table in db
   def self.all
     results = DB.exec("SELECT * FROM stylists;")
     stylists = []
@@ -13,6 +14,33 @@ class Stylist
       stylists.push(Stylist.new(name: stylist.fetch('name', id: stylist.fetch('id'))))
     end
     stylists
+  end
+
+# save data to db
+  def save
+    DB.exec("INSERT INTO stylists (name) VALUES ('#{self.name}')")
+  end
+
+# Get single ecord from db
+  def self.find(id)
+    result = DB.exec("SELECT * FROM stylists WHERE id = #{id};")
+    stylist = nil
+    result.each do |s|
+      stylist = Stylist.new(name: s.fetch('name', id: s.fetch('id')))
+    end
+    stylist
+  end
+
+# update records in db
+  def update(attributes)
+    @name = attributes.fetch('name')
+    @id = self.id
+    DB.exec("UPDATE stylists SET name = '#{@name}' WHERE id = #{@id};")
+  end
+
+# delete recoed from tanle stylists
+  def delete
+    DB.exec("DELETE FROM stylists WHERE id = #{self.id};")
   end
 
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -13,9 +13,51 @@ end
 describe(Stylist) do
 
   describe('.all') do
-    it('starts off with no stylists') do
+    xit('starts off with no stylists') do
       expect(Stylist.all).to(eq([]))
     end
   end
+
+  describe(".find") do
+      xit("returns a Stylist by her ID number") do
+        test_stylist = Stylist.new({:name => "Sam", :id => nil})
+        test_stylist.save()
+        test_stylist2 = Stylist.new({:name => "Paul", :id => nil})
+        test_stylist2.save()
+        expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2))
+      end
+    end
+
+    describe("#==") do
+      xit("is the same Stylist if it has the same name and id") do
+        Stylist = Stylist.new({:name => "Oceans Eleven", :id => nil})
+        Stylist2 = Stylist.new({:name => "Oceans Eleven", :id => nil})
+        expect(Stylist).to(eq(Stylist2))
+      end
+    end
+
+    describe("#update") do
+      xit("lets you update Stylists in the database") do
+        Stylist = Stylist.new({:name => "Oceans Eleven", :id => nil})
+        actor = Actor.new({:name => "Rami Malek", :id => nil})
+        actor1 = Actor.new({:name => "Brad Pitt", :id => nil})
+        Stylist.save()
+        actor.save()
+        actor1.save()
+        Stylist.update({:actor_ids => [actor.id, actor1.id]})
+        expect(Stylist.actors()).to(eq([actor,actor1]))
+      end
+    end
+
+    describe("#delete") do
+      xit("lets you delete a Stylist from the database") do
+        stylist = Stylist.new({:name => "Sam", :id => nil})
+        stylist.save()
+        stylist2 = Stylist.new({:name => "Paul", :id => nil})
+        stylist2.save()
+        stylist.delete()
+        expect(Stylist.all()).to(eq([Stylist2]))
+      end
+    end
 
 end
